7cb17ef88516c6b3046494b9f8ec34db07d5fe05
Dylan Avery <fossilfuelmusic@gmail.com>
2017-01-01 18:37:52 -0700

    finish sass, some linting

--- lib/Card.jsx
+++ lib/Card.jsx
@@ -7,8 +7,11 @@ export default class Card extends React.Component {
     return (
       <article className='cards'>
         <h4 className="underlines">{this.props.title} (GitHub)</h4>
-        <a href={'http://github.com/' + this.props.link} target="_blank">
-        <img className='screens' id={this.props.id} src={this.props.screenshot} alt={this.props.id}/></a>
+        <a href={`http://github.com/ + ${this.props.link}`} target='_blank'>
+        <img className='screens'
+        id={this.props.id}
+        src={this.props.screenshot}
+        alt={this.props.id}/></a>
         <p>{this.props.description}</p>
       </article>
 )
